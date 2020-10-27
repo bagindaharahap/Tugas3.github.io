533734473d94bf1f70a2117fba2f65f3c880afab
bagindaharahap <72502200+bagindaharahap@users.noreply.github.com>
2020-10-27 12:06:24 +0700

    Upload tugas 3 proweb
    upoad tugas 3 proweb

---
+++ tugas3final.js
@@ -0,0 +1,39 @@
+function kurang () {
+    a = document.register.a.value;
+    b = document.register.b.value;
+
+    kurang = a-b; 
+
+    alert("Hasilnya adalah:" +kurang);
+    location.reload();
+    
+}
+function tambah () {
+    a = document.register.a.value;
+    b = document.register.b.value;
+
+    tambah = Number(a) + Number(b);
+
+    alert("Hasilnya adalah:" +tambah);
+    location.reload();
+}
+function kali () {
+    a = document.register.a.value;
+    b = document.register.b.value;
+
+    kali = a*b; 
+
+    alert("Hasilnya adalah:" +kali);
+    location.reload();
+    
+}
+function bagi () {
+    a = document.register.a.value;
+    b = document.register.b.value;
+
+    bagi = a/b; 
+
+    alert("Hasilnya adalah:" +bagi);
+    location.reload();
+    
+}
